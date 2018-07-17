1fd0f0890a07c5aa861a5aa6b47042993c23a3a1
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-07-17 19:27:46 +0300

    Added semicolons with according to JavaScript codestyle guide

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var TIMEOUT_IN_SECS = 3 * 60
-var TEMPLATE = '<h1><span class="js-timer-minutes">00</span>:<span class="js-timer-seconds">00</span></h1>'
+var TIMEOUT_IN_SECS = 3 * 60;
+var TEMPLATE = '<h1><span class="js-timer-minutes">00</span>:<span class="js-timer-seconds">00</span></h1>';
 
 function padZero(number){
   return ("00" + String(number)).slice(-2);
@@ -9,37 +9,37 @@ class Timer{
   // IE does not support new style classes yet
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
   constructor(timeout_in_secs){
-    this.initial_timeout_in_secs = timeout_in_secs
-    this.reset()
+    this.initial_timeout_in_secs = timeout_in_secs;
+    this.reset();
   }
   getTimestampInSecs(){
-    var timestampInMilliseconds = new Date().getTime()
-    return Math.round(timestampInMilliseconds/1000)
+    var timestampInMilliseconds = new Date().getTime();
+    return Math.round(timestampInMilliseconds/1000);
   }
   start(){
     if (this.isRunning)
-      return
-    this.timestampOnStart = this.getTimestampInSecs()
-    this.isRunning = true
+      return;
+    this.timestampOnStart = this.getTimestampInSecs();
+    this.isRunning = true;
   }
   stop(){
     if (!this.isRunning)
-      return
-    this.timeout_in_secs = this.calculateSecsLeft()
-    this.timestampOnStart = null
-    this.isRunning = false
+      return;
+    this.timeout_in_secs = this.calculateSecsLeft();
+    this.timestampOnStart = null;
+    this.isRunning = false;
   }
   reset(timeout_in_secs){
-    this.isRunning = false
-    this.timestampOnStart = null
-    this.timeout_in_secs = this.initial_timeout_in_secs
+    this.isRunning = false;
+    this.timestampOnStart = null;
+    this.timeout_in_secs = this.initial_timeout_in_secs;
   }
   calculateSecsLeft(){
     if (!this.isRunning)
-      return this.timeout_in_secs
-    var currentTimestamp = this.getTimestampInSecs()
-    var secsGone = currentTimestamp - this.timestampOnStart
-    return Math.max(this.timeout_in_secs - secsGone, 0)
+      return this.timeout_in_secs;
+    var currentTimestamp = this.getTimestampInSecs();
+    var secsGone = currentTimestamp - this.timestampOnStart;
+    return Math.max(this.timeout_in_secs - secsGone, 0);
   }
 }
 
@@ -47,66 +47,66 @@ class TimerWidget{
   // IE does not support new style classes yet
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
   construct(){
-    this.timerContainer = this.minutes_element = this.seconds_element = null
+    this.timerContainer = this.minutes_element = this.seconds_element = null;
   }
   mount(rootTag){
     if (this.timerContainer)
-      this.unmount()
+      this.unmount();
 
     // adds HTML tag to current page
-    this.timerContainer = document.createElement('div')
+    this.timerContainer = document.createElement('div');
 
-    this.timerContainer.setAttribute("style", "height: 100px;")
-    this.timerContainer.innerHTML = TEMPLATE
+    this.timerContainer.setAttribute("style", "height: 100px;");
+    this.timerContainer.innerHTML = TEMPLATE;
 
-    rootTag.insertBefore(this.timerContainer, rootTag.firstChild)
+    rootTag.insertBefore(this.timerContainer, rootTag.firstChild);
 
-    this.minutes_element = this.timerContainer.getElementsByClassName('js-timer-minutes')[0]
-    this.seconds_element = this.timerContainer.getElementsByClassName('js-timer-seconds')[0]
+    this.minutes_element = this.timerContainer.getElementsByClassName('js-timer-minutes')[0];
+    this.seconds_element = this.timerContainer.getElementsByClassName('js-timer-seconds')[0];
   }
   update(secsLeft){
     var minutes = Math.floor(secsLeft / 60);
     var seconds = secsLeft - minutes * 60;
 
-    this.minutes_element.innerHTML = padZero(minutes)
-    this.seconds_element.innerHTML = padZero(seconds)
+    this.minutes_element.innerHTML = padZero(minutes);
+    this.seconds_element.innerHTML = padZero(seconds);
   }
   unmount(){
     if (!this.timerContainer)
-      return
-    this.timerContainer.remove()
-    this.timerContainer = this.minutes_element = this.seconds_element = null
+      return;
+    this.timerContainer.remove();
+    this.timerContainer = this.minutes_element = this.seconds_element = null;
   }
 }
 
 
 function main(){
 
-  var timer = new Timer(TIMEOUT_IN_SECS)
-  var timerWiget = new TimerWidget()
-  var intervalId = null
+  var timer = new Timer(TIMEOUT_IN_SECS);
+  var timerWiget = new TimerWidget();
+  var intervalId = null;
 
-  timerWiget.mount(document.body)
+  timerWiget.mount(document.body);
 
   function handleIntervalTick(){
-    var secsLeft = timer.calculateSecsLeft()
-    timerWiget.update(secsLeft)
+    var secsLeft = timer.calculateSecsLeft();
+    timerWiget.update(secsLeft);
   }
 
   function handleVisibilityChange(){
     if (document.hidden) {
-      timer.stop()
-      clearInterval(intervalId)
-      intervalId = null
+      timer.stop();
+      clearInterval(intervalId);
+      intervalId = null;
     } else {
-      timer.start()
-      intervalId = intervalId || setInterval(handleIntervalTick, 300)
+      timer.start();
+      intervalId = intervalId || setInterval(handleIntervalTick, 300);
     }
   }
 
   // https://developer.mozilla.org/en-US/docs/Web/API/Page_Visibility_API
   document.addEventListener("visibilitychange", handleVisibilityChange, false);
-  handleVisibilityChange()
+  handleVisibilityChange();
 }
 
 if (document.readyState === "complete" || document.readyState === "loaded") {
